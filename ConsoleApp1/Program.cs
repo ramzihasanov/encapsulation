7e138c47edc2a0421754991eea7c322e5745624b
ramzihasanov <bd54tkjwr@code.edu.az>
2023-10-10 10:59:28 +0400

    class and encapsulation

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static string Capitalize(string word)
+        {
+            char uperchar;
+           
+                if (word[0] <= 'z' && word[0] >= 'a')
+                {
+                    uperchar = (char)(word[0] - 32);
+                }
+
+                else uperchar = word[0];
+            
+            return uperchar + word[1..];
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            person person = new person("Remzi Hesenov", 19, "0602646414");
+            person.ShowFullName();
+
+            Console.WriteLine("==============2ci task=========================");
+            Console.WriteLine(Capitalize("mellim"));
+
+            
+        }
+    }
+}
---
+++ ConsoleApp1/person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    public class person
+    {
+        public person(string fullName,byte age,string phoneNumber)
+        {
+            FullName = fullName;
+            Age = age;
+
+
+        }
+        private string _fullName;
+        public string PhoneNumber { get; set; }
+
+        public string FullName {
+            get=>_fullName;
+            set
+            {
+                
+                if (IsFullName(value))
+                {
+                    _fullName = value;
+                }
+                else
+                {
+                    Console.WriteLine("fulname duzgun deyil");
+                }
+                
+
+            }
+        }
+
+
+        public bool IsFullName(string fulname)
+        {
+            string[] words = fulname.Split(' ');
+
+            if (words.Length >= 2)
+            {
+                foreach (string word in words)
+                {
+                    if (string.IsNullOrWhiteSpace(word) || !char.IsUpper(word[0]))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            return false;
+        }
+        private byte _age;
+        public byte Age { get=>_age; set { if (value > 0&& value <150) _age = value; } }
+
+
+        public void ShowFullName()
+        {
+            Console.WriteLine($"fullname:{_fullName}\nage:{_age}");
+        }
+
+    }
+}
